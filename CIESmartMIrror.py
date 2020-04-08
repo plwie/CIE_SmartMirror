81a56ca15311f44d712ec16e8d99f126a957c766
plwie <58283153+plwie@users.noreply.github.com>
2020-04-08 20:22:01 +0700

    Update CIESmartMIrror.py

--- CIESmartMIrror.py
+++ CIESmartMIrror.py
@@ -1,14 +1,24 @@
 import sys
-import time
 import calendar
+import requests
+import urllib.request
+import time
+import json
+import pytz
 from datetime import datetime
 from PyQt5.QtWidgets import QApplication, QWidget, QCalendarWidget, QHBoxLayout, QVBoxLayout, QLabel
 from PyQt5.QtCore import QDate, QTime, QTimer, Qt
 from PyQt5.QtGui import QFont, QPalette, QIcon
 
+
+#For clock
 date_format = "%b %d, %Y"
 time_format = 12
 
+#For Weather
+url = 'https://api.darksky.net/forecast/75bcc50aef1d5aec09148f2849c50db5/13.8816,100.6445/?units=si'
+response = requests.get(url)
+result = response.json()
 
 class Window(QWidget):
     def __init__(self,*args,**kwargs):
@@ -31,13 +41,15 @@ class Window(QWidget):
         self.calendar.setFixedHeight(150)
         self.clock = Clock()
         self.clock.setFixedHeight(150)
+        self.weather = Weather()
+        self.weather.setFixedHeight(150)
+        Hbox1.addWidget(self.weather)
         Hbox1.addWidget(self.clock)
         Hbox1.addWidget(self.calendar)
         self.setLayout(Hbox1)
         
         
         self.show()
-
 
 class Clock(QWidget):
     def __init__(self, *args, **kwargs):
@@ -100,23 +112,71 @@ class Calendar(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.calendar = QCalendarWidget(self)
         self.setWindowTitle('Calendar')
         self.resize(350,220)
         self.initUI()
 
     def initUI(self):
-        self.calendar = QCalendarWidget(self)
-        self.calendar.NoVerticalHeader
         self.calendar.move(20, 20)
         self.calendar.setGridVisible(True)
-        
-
+    
         self.calendar.setMinimumDate(QDate(currentYear, currentMonth - 1, 1))
         self.calendar.setMaximumDate(QDate(currentYear, currentMonth + 1, calendar.monthrange(currentYear, currentMonth)[1]))
 
     
+class Weather(QWidget):
+    def __init__(self, *args, **kwargs):
+        super(Weather, self).__init__()
+        self.initUI()
 
-    
+    def initUI(self):
+        font1 = QFont('Bavaria', 20)
+        font2 = QFont('Bavaria', 10)
+        
+        #Vbox
+        self.vbox= QVBoxLayout()
+        self.vbox1 = QVBoxLayout()
+        self.vbox2 = QVBoxLayout()
+        self.vbox3 = QVBoxLayout()
+        #Location
+        self.location = QLabel('Bangkok')
+        self.location.setFont(font2)
+        self.vbox1.addWidget(self.location)
+        #Temperature
+        degree_sign= u'\N{DEGREE SIGN}'
+        temperature = str(int(result['currently']['temperature']))
+        temperature2 = "%s%s" % (temperature, degree_sign)
+        self.temperature = QLabel(temperature2)
+        self.temperature.setFont(font1)
+        self.vbox2.addWidget(self.temperature)
+        #Current
+        current = str(result['currently']['summary'])
+        self.current = QLabel(current)
+        self.current.setFont(font1)
+        self.vbox3.addWidget(self.current)
+        # #Forecast
+        # self.forecastLbl = QLabel('forecast')
+        # self.forecastLbl.setFont(font1)
+        # self.vbox2.addWidget(self.forecastLbl)
+
+        self.vbox2.addStretch(1)
+
+        self.vbox.addLayout(self.vbox1)
+        self.vbox.addLayout(self.vbox2)
+        self.vbox.addLayout(self.vbox3)
+        self.vbox.setContentsMargins(0,0,0,0)
+        self.setLayout(self.vbox)
+
+    def time_convert(timezone,unix_time):
+        # get time in tz
+        tz = pytz.timezone(timezone)
+        dt = datetime.fromtimestamp(unix_time, tz)
+        # print it
+        return(dt.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Window()
